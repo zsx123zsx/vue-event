87795d5ebb2b7d2d9d2ffdc8d4cb02d8e5bb128c
zsx <619893077@qq.com>
2022-10-07 00:55:00 +0800

    项目初始化

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,17 +1,19 @@
 module.exports = {
   root: true,
   env: {
-    node: true
+    node: true,
   },
-  extends: [
-    'plugin:vue/essential',
-    '@vue/standard'
-  ],
+  extends: ['plugin:vue/essential', '@vue/standard'],
   parserOptions: {
-    parser: '@babel/eslint-parser'
+    parser: '@babel/eslint-parser',
   },
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
-  }
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'comma-dangle': ['warn', 'never'],
+    // 要求或禁止函数圆括号之前有一个空格
+    'space-before-function-paren': 0,
+    // 出现多行空格
+    'no-multiple-empty-lines': 0,
+  },
 }
